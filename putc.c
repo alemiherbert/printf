4c20da12a755e277f998f12bdbd60de15d95bcd4
Alemi Herbert <alemiherbert@gmail.comt>
2022-08-10 02:29:54 +0300

    Added putc()

---
+++ putc.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+
+/**
+ * _putc - function that prints a character
+ * @c: the character to print
+ * Return: -1 if failed
+ */
+int _putc(char c)
+{
+    return (write(1, &c, 1));
+}
