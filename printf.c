f2e398a207bcedec59fc038a7e9d3bd367724514
Alemi Herbert <alemiherbert@gmail.comt>
2022-08-10 02:25:05 +0300

    Set up variadic fn requirements

---
+++ printf.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+
+/**
+ * _printf - function that prints a formatted string
+ * @format: the string to be printed
+ * 
+ * Return: number of chars printed
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int length = 0;
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(args, format);
+
+	length = _vprintf(format, args);
+	va_end(args);
+	return (length);
+}
