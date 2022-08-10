57c692646e87ec89e9f84e068e4727ecae71a4bf
Alemi Herbert <alemiherbert@gmail.comt>
2022-08-10 03:56:49 +0300

    Added integer specifiers

--- vprintf.c
+++ vprintf.c
@@ -63,7 +63,8 @@ int _print_spec(char format, va_list args)
 	fmt_spec _types[] = {
 		{"c", _print_char},
 		{"s", _print_string},
-
+		{"d", _print_integer},
+		{"i", _print_integer},
 		{NULL, NULL}
 	};
 
@@ -111,7 +112,7 @@ int _print_invalid_spec(char prev_format, char format, int count)
  */
 int _check_char(char _type)
 {
-	char _types[] = {'c', 's', '%'};
+	char _types[] = {'c', 's', 'd', 'i', '%'};
 	int i = 0;
 
 	while (_types[i])
