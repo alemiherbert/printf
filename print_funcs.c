18b976de3cbb012147249edf630224d8f5b1e3f3
Alemi Herbert <alemiherbert@gmail.comt>
2022-08-10 04:11:13 +0300

    Added int - binary handler function:

--- print_funcs.c
+++ print_funcs.c
@@ -79,3 +79,48 @@ void _recursion_integer(int a)
 		_recursion_integer(t / 10);
 	_putc(t % 10 + '0');
 }
+
+/**
+  * _print_int_binary - Prints a int converted to binary
+  * @args: A list of variadic arguments
+  *
+  * Return: The number of printed digits
+  */
+int _print_int_binary(va_list args)
+{
+	unsigned int x = 0;
+	int b = 0, aux = 0;
+
+	aux = va_arg(args, int);
+	x = aux;
+	if (aux < 0)
+	{
+		_putc('1');
+		aux = aux * -1;
+		x = aux;
+		b += 1;
+	}
+	while (x > 0)
+	{
+		x = x / 2;
+		b++;
+	}
+	_recursion_int_binary(aux);
+	return (b);
+}
+
+/**
+  * _recursion_int_binary - Prints a binary number
+  * @a: integer to print
+  *
+  * Return: Nothing
+  */
+void _recursion_int_binary(int a)
+{
+	unsigned int t;
+
+	t = a;
+	if (t / 2)
+		_recursion_int_binary(t / 2);
+	_putc(t % 2 + '0');
+}
