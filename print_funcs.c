31a4f754b5cb783b6eb079747c24ddc57129d41c
Alemi Herbert <alemiherbert@gmail.comt>
2022-08-10 03:50:47 +0300

    Added integer handling

--- print_funcs.c
+++ print_funcs.c
@@ -32,3 +32,50 @@ int _print_string(va_list args)
     }
     return (i);
 }
+
+/**
+  * _print_a_integer - prints a integer
+  * @args: list of variadic arguments
+  *
+  * Return: length of the string
+  */
+int _print_a_integer(va_list args)
+{
+	int count = 1, val = 0;
+	unsigned int aux = 0;
+
+    /* copy the integer into an auxilliary variable */
+	aux = va_arg(args, int);
+	val = aux;
+	if (val < 0)
+	{
+		_putc('-');
+		val = val * -1;
+		aux = val;
+		count ++;
+	}
+	while (aux > 9)
+	{
+		aux = aux / 10;
+		count++;
+	}
+
+	_recursion_integer(val);
+	return (count);
+}
+
+/**
+  * _recursion_integer - print an integer
+  * @a: the integer to print
+  *
+  * Return: Nothing
+  */
+void _recursion_integer(int a)
+{
+	unsigned int t;
+
+	t = a;
+	if (t / 10)
+		_recursion_integer(t / 10);
+	_putc(t % 10 + '0');
+}
